c4bc4ce3227d43b6bb71b44fc14dbb8f7366d6a6
rathodmehul6263-lang <rathodmehul6263@gmail.com>
2026-07-14 00:32:10 +0530

    build: provide fallback values for keystore passwords

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,9 +48,9 @@ android {
     create("release") {
       val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
       storeFile = file(keystorePath)
-      storePassword = System.getenv("STORE_PASSWORD")
+      storePassword = System.getenv("STORE_PASSWORD") ?: "cosmicstriker"
       keyAlias = "upload"
-      keyPassword = System.getenv("KEY_PASSWORD")
+      keyPassword = System.getenv("KEY_PASSWORD") ?: "cosmicstriker"
     }
   }
 
